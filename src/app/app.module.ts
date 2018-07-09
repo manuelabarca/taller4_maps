5d3e7a2419f324a748aae9ecd066315320f27486
Manuel Labarca <mlabarca@sentra.cl>
2018-07-09 16:04:30 -0400

    proyecto maps

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,17 @@
+
 import { BrowserModule } from '@angular/platform-browser';
 import { ErrorHandler, NgModule } from '@angular/core';
 import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
 import { SplashScreen } from '@ionic-native/splash-screen';
 import { StatusBar } from '@ionic-native/status-bar';
+import { Network } from '@ionic-native/network';
+import { Geolocation } from '@ionic-native/geolocation';
+import { GoogleMaps } from '../services/google.service';
 
 import { MyApp } from './app.component';
 import { HomePage } from '../pages/home/home';
+import { Conexion } from '../services/conexion.service';
+
 
 @NgModule({
   declarations: [
@@ -24,6 +30,10 @@ import { HomePage } from '../pages/home/home';
   providers: [
     StatusBar,
     SplashScreen,
+    Network,
+    Geolocation,
+    GoogleMaps,
+    Conexion,
     {provide: ErrorHandler, useClass: IonicErrorHandler}
   ]
 })
